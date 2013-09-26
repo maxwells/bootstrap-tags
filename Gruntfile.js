3dfc9f73fb9236713f36786856ac59d2b0857e0d
Max Lahey <maxwellslahey@gmail.com>
2013-09-25 22:37:59 -0400

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -151,7 +151,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-jst');
