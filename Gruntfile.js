820934f76b52a01f67ff8edd633bf559f639753f
bool.dev <bool.dev@gmail.com>
2014-05-15 01:42:59 +0530

    Version bumped to 1.1.5, updated corresponding grunt and bower files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
       options: {
         banner: [
           '/*!',
-          ' * bootstrap-tags 1.1.0',
+          ' * bootstrap-tags 1.1.5',
           ' * https://github.com/maxwells/bootstrap-tags',
           ' * Copyright 2013 Max Lahey; Licensed MIT',
           ' */\n\n'
